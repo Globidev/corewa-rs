ee0b1e6437beff732b0254f406046210ab440bf1
Guillaume Depardon <guillaume.depardon@gmail.com>
2019-10-27 02:34:38 +0200

    Started implementing the tui arena

--- corewa-rs-term-arena/src/main.rs
+++ corewa-rs-term-arena/src/main.rs
@@ -1,23 +1,53 @@
-use corewa_rs::vm::VirtualMachine;
-use structopt::StructOpt;
+mod util;
 
-#[derive(Debug, StructOpt)]
-struct Options {
-    champion_files: Vec<String>
+use std::{error::Error, io};
+use termion::{
+    event::Key, input::MouseTerminal, raw::IntoRawMode, screen::AlternateScreen,
+};
+use tui::{
+    backend::TermionBackend,
+    buffer::Buffer,
+    layout::{Constraint, Direction, Layout, Rect},
+    style::{Style, Color},
+    widgets::{Block, Borders, Widget},
+    Terminal,
+};
+use util::{Event, Events};
+
+fn main() {
+    let exit_code = match run() {
+        Ok(_) => 0,
+        Err(err) => {
+            println!("{}", err);
+            1
+        }
+    };
+
+    std::process::exit(exit_code)
 }
 
-fn main() -> Result<(), io::Error> {
+fn run() -> Result<(), Box<dyn Error>> {
     let opts = Options::from_args();
-
-    println!("{:?}", opts);
 
     if opts.champion_files.is_empty() {
         panic!("Require at least 1 champion");
     }
 
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    let events = Events::new();
+
+
     let mut vm = VirtualMachine::new();
 
-    let players: Vec<_> = opts.champion_files.iter()
+    let players: Vec<_> = opts
+        .champion_files
+        .iter()
         .enumerate()
         .map(|(i, file_name)| {
             let champion = read_cor_file(&file_name)?;
@@ -27,19 +57,223 @@ fn main() -> Result<(), io::Error> {
 
     vm.load_players(&players);
 
-    while !vm.processes.is_empty() {
-        vm.tick();
-        if vm.cycles % 500 == 0 {
-            println!("{}", vm.cycles);
-            println!("{}", vm.processes.len());
+    let colors = [
+        Color::Yellow,
+        Color::Magenta,
+        Color::Green,
+        Color::Cyan,
+    ];
+
+    let player_colors = vm.players.iter().enumerate()
+        .map(|(idx, player)| (player.id, colors[idx]))
+        .collect();
+
+    let mut controls = Controls::default();
+
+    loop {
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(80),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+
+            Block::default()
+                .borders(Borders::ALL)
+                .title("VM state")
+                .render(&mut f, chunks[0]);
+
+            let info_chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(20),
+                        Constraint::Percentage(80),
+                    ]
+                    .as_ref()
+                )
+                .split(chunks[0]);
+
+            Block::default()
+                .borders(Borders::BOTTOM)
+                .render(&mut f, info_chunks[0]);
+
+            Block::default()
+                .borders(Borders::ALL)
+                .title("Memory")
+                .render(&mut f, chunks[1]);
+
+            let vm_chunks = Layout::default()
+                .constraints([Constraint::Percentage(100)].as_ref())
+                .margin(1)
+                .split(chunks[1]);
+
+            controls.render(&mut f, info_chunks[0]);
+
+            VMStateWidget(&vm)
+                .render(&mut f, info_chunks[1]);
+
+            MemoryWidget(&vm, &player_colors, opts.chr)
+                .render(&mut f, vm_chunks[0]);
+
+        })?;
+
+        match events.next()? {
+            Event::Key(key) => {
+                match key {
+                    Key::Char(c) => {
+                        match c {
+                            'q' => break,
+                            '+' => controls.faster(),
+                            '-' => controls.slower(),
+                            ' ' => controls.toggle_running(),
+                            'r' => { vm = VirtualMachine::new(); vm.load_players(&players) },
+                            _ => ()
+                        }
+                    },
+                    Key::Right => vm.tick(),
+                    _ => ()
+                }
+            },
+            Event::Mouse(_ev) => {
+                // dbg!(ev);
+            },
+            Event::Tick => {
+                if controls.running {
+                    for _ in 0..controls.speed {
+                        vm.tick();
+                    }
+                }
+            }
         }
     }
 
     Ok(())
 }
 
-use std::fs::File;
-use std::io::{self, Read};
+struct Controls {
+    speed: u16,
+    running: bool,
+}
+
+impl Default for Controls {
+    fn default() -> Self {
+        Self {
+            speed: 1,
+            running: true,
+        }
+    }
+}
+
+impl Controls {
+    fn faster(&mut self) {
+        self.speed = (self.speed * 2).min(128)
+    }
+
+    fn slower(&mut self) {
+        self.speed = (self.speed / 2).max(1)
+    }
+
+    fn toggle_running(&mut self) {
+        self.running = !self.running
+    }
+}
+
+impl Widget for Controls {
+    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
+        let running_string = if self.running { "running" } else { "stopped" };
+        buf.set_string(area.left(), area.top(), running_string, Style::default());
+        buf.set_string(area.left(), area.top() + 1, format!("Speed: x{}", self.speed), Style::default());
+    }
+}
+
+struct VMStateWidget<'a>(&'a VirtualMachine);
+
+impl Widget for VMStateWidget<'_> {
+    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
+        let mut line_offset = 0;
+        let mut show_line = |text| {
+            let x = area.left();
+            let y = area.top() + line_offset;
+            line_offset += 1;
+            buf.set_string(x, y, text, Style::default());
+        };
+
+        let vm = &self.0;
+
+        let next_check = vm.check_interval - (vm.cycles - vm.last_live_check);
+
+        show_line(format!("Cycles:         {}", vm.cycles));
+        show_line(format!("Process:        {}", vm.processes.len()));
+        show_line(format!("Check interval: {}", vm.check_interval));
+        show_line(format!("Next check:     {}", next_check));
+        show_line(format!("Last check:     {}", vm.last_live_check));
+        show_line(format!("Live count:     {}", vm.live_count_since_last_check));
+        show_line(format!("Checks passed:  {}", vm.checks_without_cycle_decrement));
+    }
+}
+
+struct MemoryWidget<'a>(&'a VirtualMachine, &'a PlayerColors, char);
+
+type PlayerColors = HashMap<PlayerId, Color>;
+
+impl<'a> Widget for MemoryWidget<'a> {
+    fn draw(&mut self, area: Rect, buf: &mut Buffer) {
+        let mem = &self.0.memory;
+        let width = 128.min(area.right() - area.left());
+
+        for idx in 0..mem.size() {
+            let y = idx as u16 / width;
+            let x = idx as u16 % width;
+
+            if area.top() + y >= area.bottom() {
+                break
+            }
+
+            let cell = buf.get_mut(area.left() + x, area.top() + y);
+
+            let player_id = mem.owners[idx];
+            let color = if player_id != 0 {
+                self.1[&player_id]
+            } else {
+                Color::DarkGray
+            };
+
+            let pc_count = self.0.process_count_per_cells[idx];
+
+            let ch = match pc_count {
+                0 => self.2,
+                c@1..=9 => char::from(b'0' + c as u8),
+                _ => '+',
+            };
+            cell.set_char(ch);
+            cell.set_fg(color);
+            if pc_count >= 1 {
+                cell.set_bg(Color::DarkGray);
+            }
+        }
+    }
+}
+
+use corewa_rs::vm::{types::PlayerId, VirtualMachine};
+use structopt::StructOpt;
+use std::collections::HashMap;
+
+#[derive(Debug, StructOpt)]
+struct Options {
+    champion_files: Vec<String>,
+    #[structopt(short = "c", default_value = "▮")]
+    chr: char,
+}
+
+use std::{fs::File, io::Read};
 
 fn read_cor_file(file_name: &str) -> Result<Vec<u8>, io::Error> {
     let mut data = Vec::with_capacity(8192);
---
+++ corewa-rs-term-arena/src/util.rs
@@ -0,0 +1,89 @@
+use std::io;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+use termion::event::{Key, Event as TermEvent, MouseEvent};
+use termion::input::TermRead;
+
+pub enum Event {
+    Key(Key),
+    Mouse(MouseEvent),
+    Tick,
+}
+
+/// A small event handler that wrap termion input and tick events. Each event
+/// type is handled in its own thread and returned to a common `Receiver`
+pub struct Events {
+    rx: mpsc::Receiver<Event>,
+    _input_handle: thread::JoinHandle<()>,
+    _tick_handle: thread::JoinHandle<()>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Config {
+    pub exit_key: Key,
+    pub tick_rate: Duration,
+}
+
+impl Default for Config {
+    fn default() -> Config {
+        Config {
+            exit_key: Key::Char('q'),
+            tick_rate: Duration::from_millis(16),
+        }
+    }
+}
+
+impl Events {
+    pub fn new() -> Events {
+        Events::with_config(Config::default())
+    }
+
+    pub fn with_config(config: Config) -> Events {
+        let (tx, rx) = mpsc::channel();
+        let input_handle = {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let stdin = io::stdin();
+                for evt in stdin.events().flatten() {
+                    match evt {
+                        TermEvent::Key(key) => {
+                            if let Err(_) = tx.send(Event::Key(key)) {
+                                return;
+                            }
+                            if key == config.exit_key {
+                                return;
+                            }
+                        },
+                        TermEvent::Mouse(ev) => {
+                            if let Err(_) = tx.send(Event::Mouse(ev)) {
+                                return;
+                            }
+                        },
+                        _ => {}
+                    }
+                }
+            })
+        };
+        let tick_handle = {
+            let tx = tx.clone();
+            thread::spawn(move || {
+                let tx = tx.clone();
+                loop {
+                    tx.send(Event::Tick).unwrap();
+                    thread::sleep(config.tick_rate);
+                }
+            })
+        };
+        Events {
+            rx,
+            _input_handle: input_handle,
+            _tick_handle: tick_handle,
+        }
+    }
+
+    pub fn next(&self) -> Result<Event, mpsc::RecvError> {
+        self.rx.recv()
+    }
+}
