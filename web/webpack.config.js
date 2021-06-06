3ebea9bddad83dded615f91b809801f809c99513
Guillaume Depardon <guillaume.depardon@gmail.com>
2021-06-06 15:12:43 +0200

    Fixed source maps

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -31,6 +31,11 @@ module.exports = {
           'css-loader',
         ],
       },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ],
   },
 
