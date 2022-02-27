0e89e780ba7f01c080c10d4311f26a202c172e00
Guillaume Depardon <guillaume.depardon@gmail.com>
2022-02-27 14:11:35 +0100

    Switch to a functional component for the vm viewer

--- web/src/components/vm.tsx
+++ web/src/components/vm.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { observer } from "mobx-react";
-import { action, makeObservable, observable, observe, reaction } from "mobx";
+import { useEffect, useRef } from "react";
+import { observer, useLocalObservable } from "mobx-react";
+import { action, observe, reaction } from "mobx";
 
 import { VirtualMachine } from "../virtual_machine";
 import { PIXIRenderer, MARGIN, MEM_HEIGHT, MEM_WIDTH } from "../renderer";
@@ -25,131 +25,109 @@ interface IVMProps {
   onHelpRequested: () => void;
 }
 
-@observer
-export class VM extends React.Component<IVMProps> {
-  canvasRef = React.createRef<HTMLCanvasElement>();
+export const VM = observer(
+  ({ vm, onHelpRequested, onNewPlayerRequested }: IVMProps) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  selections = new Map<number, Selection>();
+    const selections = useLocalObservable(() => new Map<number, Selection>());
+    const coverages = useLocalObservable(() => new Map<number, number>());
 
-  coverages = new Map<number, number>();
+    const selectionAt = (idx: number) => {
+      return {
+        decoded: vm.engine.decode(idx),
+        processes: vm.engine.processes_at(idx),
+      };
+    };
 
-  vm = this.props.vm;
+    const onNewClicked = () => {
+      if (vm.playersById.size < 4) onNewPlayerRequested();
+    };
 
-  constructor(props: IVMProps) {
-    super(props);
-    makeObservable(this, {
-      selections: observable,
-      draw: action,
-      toggleSelection: action,
-      discardSelection: action,
-      updateSelections: action,
-      clearSelections: action,
+    const draw = action((renderer: PIXIRenderer) => {
+      const memory = vm.engine.memory();
+
+      renderer.update({
+        memory,
+        selections: Array.from(selections).map(([idx, selection]) => ({
+          idx,
+          length: Math.max(selection.decoded.byte_size(), 1),
+        })),
+        playersById: vm.playersById,
+      });
+
+      const cellOwners = new Int32Array(
+        vm.wasmMemory.buffer,
+        memory.owners_ptr,
+        4096
+      );
+
+      coverages.clear();
+      cellOwners.forEach((owner) => {
+        const previous = coverages.get(owner) ?? 0;
+        coverages.set(owner, previous + 1);
+      });
     });
-  }
 
-  componentDidMount() {
-    const canvas = this.canvasRef.current;
+    const discardSelection = action((idx: number) => {
+      selections.delete(idx);
+    });
 
-    if (canvas) {
+    const clearSelections = action(() => {
+      selections.clear();
+    });
+
+    const updateSelections = action(() => {
+      selections.forEach((selection, idx) =>
+        Object.assign(selection, selectionAt(idx))
+      );
+    });
+
+    const toggleSelection = action((idx: number) => {
+      if (selections.has(idx)) selections.delete(idx);
+      else selections.set(idx, selectionAt(idx));
+    });
+
+    useEffect(() => {
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+
       const renderer = new PIXIRenderer(
         {
           canvas,
           onCellClicked: (cellIdx, modifiers) => {
-            if (!modifiers.ctrl) this.clearSelections();
-            this.toggleSelection(cellIdx);
+            if (!modifiers.ctrl) clearSelections();
+            toggleSelection(cellIdx);
           },
           onLoad: () => {
-            observe(this.vm, "cycles", (_) => {
-              this.updateSelections();
-              this.draw(renderer);
+            observe(vm, "cycles", (_) => {
+              updateSelections();
+              draw(renderer);
             });
-            this.draw(renderer);
+            draw(renderer);
           },
         },
-        this.props.vm.wasmMemory
+        vm.wasmMemory
       );
 
       reaction(
-        () => this.selections.size,
-        () => this.draw(renderer)
+        () => selections.size,
+        () => draw(renderer)
       );
-    }
-  }
-
-  draw(renderer: PIXIRenderer) {
-    const memory = this.vm.engine.memory();
-
-    renderer.update({
-      memory,
-      selections: Array.from(this.selections).map(([idx, selection]) => ({
-        idx,
-        length: Math.max(selection.decoded.byte_size(), 1),
-      })),
-      playersById: this.vm.playersById,
-    });
-
-    const cellOwners = new Int32Array(
-      this.props.vm.wasmMemory.buffer,
-      memory.owners_ptr,
-      4096
-    );
-
-    this.coverages.clear();
-    cellOwners.forEach((owner) => {
-      const previous = this.coverages.get(owner) || 0;
-      this.coverages.set(owner, previous + 1);
-    });
-  }
-
-  selectionAt(idx: number) {
-    return {
-      decoded: this.vm.engine.decode(idx),
-      processes: this.vm.engine.processes_at(idx),
-    };
-  }
-
-  toggleSelection(idx: number) {
-    if (this.selections.has(idx)) this.selections.delete(idx);
-    else this.selections.set(idx, this.selectionAt(idx));
-  }
-
-  updateSelections() {
-    this.selections.forEach((selection, idx) =>
-      Object.assign(selection, this.selectionAt(idx))
-    );
-  }
-
-  onNewClicked() {
-    if (this.vm.playersById.size < 4) this.props.onNewPlayerRequested();
-  }
-
-  discardSelection(idx: number) {
-    this.selections.delete(idx);
-  }
-
-  clearSelections() {
-    this.selections.clear();
-  }
-
-  render() {
-    const vm = this.vm;
+    }, []);
 
     const helpButton = (
-      <button
-        className="ctrl-btn"
-        onClick={this.props.onHelpRequested.bind(this)}
-      >
+      <button className="ctrl-btn" onClick={onHelpRequested}>
         ❓
       </button>
     );
 
     const addPlayerButton = vm.playersById.size < 4 && (
-      <button className="ctrl-btn" onClick={this.onNewClicked.bind(this)}>
+      <button className="ctrl-btn" onClick={onNewClicked}>
         ➕
       </button>
     );
 
-    const selectionsAsArray = Array.from(this.selections);
+    const selectionsAsArray = Array.from(selections);
     const selectionPanels = selectionsAsArray.map(
       ([cellIdx, selection], idx) => (
         <div key={idx}>
@@ -158,7 +136,7 @@ export class VM extends React.Component<IVMProps> {
             idx={cellIdx}
             previousIdx={idx > 0 ? selectionsAsArray[idx - 1][0] : null}
             decoded={selection.decoded}
-            onDiscard={() => this.discardSelection(cellIdx)}
+            onDiscard={() => discardSelection(cellIdx)}
           />
           <div className="pad-top">
             <ProcessPanel processes={selection.processes} vm={vm} />
@@ -169,7 +147,7 @@ export class VM extends React.Component<IVMProps> {
 
     const arena = (
       <canvas
-        ref={this.canvasRef}
+        ref={canvasRef}
         width={MEM_WIDTH}
         height={MEM_HEIGHT}
         style={{
@@ -193,7 +171,7 @@ export class VM extends React.Component<IVMProps> {
             <hr />
             <StatePanel vm={vm} />
             <hr />
-            <ContendersPanel vm={vm} coverages={this.coverages} />
+            <ContendersPanel vm={vm} coverages={coverages} />
             {selectionPanels}
           </div>
           {arena}
@@ -201,4 +179,4 @@ export class VM extends React.Component<IVMProps> {
       </div>
     );
   }
-}
+);
