d72d3ad1d1829e9db8f5077041551b070143e658
Guillaume Depardon <guillaume.depardon@gmail.com>
2022-03-06 16:49:50 +0100

    Added Roboto fonts

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -13,10 +13,18 @@ import { VirtualMachine } from "./state/vm";
 
 import { CorewarLayout } from "./components/layout";
 
+import FontFaceObserver from "fontfaceobserver";
+
 import initWasm from "corewa-rs";
 
 async function main() {
   const { memory } = await initWasm();
+
+  // We preload fonts to avoid surprises when rendering text in the canvas
+  await new FontFaceObserver("Roboto Mono").load().then(
+    () => console.debug("Fonts loaded"),
+    () => console.error("Fonts not loaded")
+  );
 
   const vm = new VirtualMachine(memory);
 
