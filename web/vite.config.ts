e18a9f8faacc82b45aca2595acb0bdc170f2fda5
Guillaume Depardon <guillaume.depardon@gmail.com>
2022-03-13 11:19:03 +0100

    Removed decorator plugins

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,16 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["@babel/plugin-proposal-decorators", { legacy: true }],
-          ["@babel/plugin-proposal-class-properties", { loose: true }],
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     fs: {
       allow: ["../.."],
