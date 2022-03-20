4267984e7c45cc8d19823f4e714f9a0a55cc7724
Guillaume Depardon <guillaume.depardon@gmail.com>
2022-03-20 15:28:03 +0100

    Rework selections panel

--- web/src/components/panels/selections.tsx
+++ web/src/components/panels/selections.tsx
@@ -3,6 +3,7 @@ import { observer } from "mobx-react-lite";
 
 import { Game } from "../../state/game";
 import { CellPanel } from "./cell";
+import { Panel } from "../panel";
 import { ProcessPanel } from "./process";
 
 import type { DecodeResult, ProcessCollection } from "corewa-rs";
@@ -23,20 +24,40 @@ export const SelectionsPanel = observer(({ game, selections }: Props) => {
   });
 
   const selectionsAsArray = Array.from(selections);
-  const panels = selectionsAsArray.map(([cellIdx, selection], idx) => (
-    <div key={cellIdx}>
-      <hr />
-      <CellPanel
-        idx={cellIdx}
-        previousIdx={idx > 0 ? selectionsAsArray[idx - 1][0] : null}
-        decoded={selection.decoded}
-        onDiscard={() => discardSelection(cellIdx)}
-      />
-      <div className="pad-top">
-        <ProcessPanel processes={selection.processes} game={game} />
+  const selectionWidgets = selectionsAsArray.map(
+    ([cellIdx, selection], idx) => (
+      <div key={cellIdx}>
+        <CellPanel
+          idx={cellIdx}
+          previousIdx={idx > 0 ? selectionsAsArray[idx - 1][0] : null}
+          decoded={selection.decoded}
+          onDiscard={() => discardSelection(cellIdx)}
+        />
+        <div className="pad-top">
+          <ProcessPanel processes={selection.processes} game={game} />
+        </div>
       </div>
-    </div>
-  ));
+    )
+  );
 
-  return <>{panels}</>;
+  const showHowToTip = selections.size === 0;
+  const showAddMoreTip = selections.size === 1;
+
+  return (
+    <Panel title="Selections">
+      <div className="selections-container">
+        {selectionWidgets}
+        {showAddMoreTip && (
+          <div className="add-more-selections-tip">
+            Ctrl + click to add more selections
+          </div>
+        )}
+      </div>
+      {showHowToTip && (
+        <div className="empty-selections-tip">
+          Click on a cell to inspect bytecode and processes
+        </div>
+      )}
+    </Panel>
+  );
 });
